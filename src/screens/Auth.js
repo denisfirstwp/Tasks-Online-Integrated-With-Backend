0f658a1245f7126b7300dae8ee4b2c1f8fdb28d8
EXTREME <denisfirstwp@gmail.com>
2020-07-10 17:30:52 -0300

    145. Tela de Autenticação #03 - React-Native - Tasks Online

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -6,8 +6,11 @@ import commonStyles from '../commonStyles'
 export default class Auth extends Component {
 
     state= {
+        name:'',
         email: '',
-        password: ''
+        password: '',
+        confirmPassword:'',
+        stageNew: true // quer dizer que estou na tela de criacao de novo usuario
     }
 
     render(){
@@ -18,13 +21,25 @@ export default class Auth extends Component {
             style={styles.background}>
                 <Text style={styles.title}>Tasks</Text>
                 <View style={styles.formContainer}>
+                    <Text style={styles.subtitle}>
+                        {this.state.stageNew ? 'Crie a sua conta' : 'Informe seus dados' }
+                    </Text>
+                    {
+                        this.state.stageNew && <TextInput placeholder='Nome' value={this.state.name}
+                        style={styles.input} onChangeText={name => this.setState({ name })} />
+                    }
                     <TextInput placeholder='E-mail' value={this.state.email}
                     style={styles.input} onChangeText={email => this.setState({ email })} />
                     <TextInput placeholder='Senha' value={this.state.password}
-                    style={styles.input} onChangeText={password => this.setState({ password })} />
+                    style={styles.input} onChangeText={password => this.setState({ password })} 
+                    secureTextEntry={true} />
+                    {this.state.stageNew && 
+                    <TextInput placeholder='Confirmação de senha' value={this.state.confirmPassword}
+                    style={styles.input} onChangeText={confirmPassword => this.setState({ confirmPassword })} 
+                    secureTextEntry={true} />}
                     <TouchableOpacity>
                         <View style={styles.button}>
-                            <Text style={styles.buttonText}>Entrar</Text>
+                            <Text style={styles.buttonText}>{this.state.stageNew ? 'Registrar': 'Entrar'}</Text>
                         </View>
                     </TouchableOpacity>
                 </View>
@@ -46,6 +61,14 @@ const styles = StyleSheet.create({
         color: commonStyles.colors.secundary,
         fontSize: 70,
         marginBottom: 10
+    },
+
+    subtitle:{
+        fontFamily: commonStyles.fontFamily,
+        color: '#FFF',
+        fontSize:20,
+        marginBottom:10,
+        textAlign:'center'
     },
     formContainer: {
         backgroundColor:'rgba(0, 0, 0, 0.7)',
