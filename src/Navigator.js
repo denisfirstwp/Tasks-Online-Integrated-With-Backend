568f5ff4cee176e66b7e9cbaaadd746eaa86c928
EXTREME <denisfirstwp@gmail.com>
2020-07-11 23:18:14 -0300

    157.Drawer Navigator #01 - React-Native Tasks-Online

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,8 +1,43 @@
 import React from 'react'
 import {createAppContainer, createSwitchNavigator} from 'react-navigation'
+import {createDrawerNavigator} from 'react-navigation-drawer'
 
 import Auth from './screens/Auth'
 import TaskList from './screens/TaskList'
+
+const menuRoutes = {
+    Today:{
+        name: 'Today',
+        screen: props =><TaskList tile='Hoje' dasysAhead={0} {...props} />,
+        navigationOptions:{
+            tilte: 'Hoje'
+        }
+    },
+    Tomorrow:{
+        name: 'Tomorrow',
+        screen: props =><TaskList tile='Amanhã' dasysAhead={1} {...props} />,
+        navigationOptions:{
+            tilte: 'Amanhã'
+        }
+    },
+    Week:{
+        name: 'Week',
+        screen: props =><TaskList tile='Semana' dasysAhead={7} {...props} />,
+        navigationOptions:{
+            tilte: 'Semana'
+        }
+    },
+    Month:{
+        name: 'Month',
+        screen: props =><TaskList tile='Mês' dasysAhead={30} {...props} />,
+        navigationOptions:{
+            tilte: 'Mês'
+        }
+    },
+
+}
+
+const menuNavigator = createDrawerNavigator(menuRoutes)
 
 const mainRoutes = { // Criando as rotas
     Auth: {
@@ -11,7 +46,8 @@ const mainRoutes = { // Criando as rotas
     },
     Home: {
         name:'Home',
-        screen: TaskList
+        //screen: TaskList
+        screen:menuNavigator
     }
 }
 
